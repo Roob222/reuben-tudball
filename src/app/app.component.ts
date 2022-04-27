c8f1e1f26588d1ae7bf9b416979f96a4c6bdf4b7
Reuben Tudball <reuben_tudball@hotmail.com>
2022-04-27 01:39:13 -0400

    terminal style added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reuben-tudball';
+  isUnderscoreVisible: boolean = true;
+  public innerWidth: any;
+
+  ngOnInit(){
+    this.innerWidth = window.innerWidth;
+  }
+
+
+  startTimer(){
+    setInterval(() => {
+      if(this.isUnderscoreVisible){
+        this.isUnderscoreVisible = false;
+      } else{
+        this.isUnderscoreVisible = true;
+      }
+    },2000);
+  }
+
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any){
+    this.innerWidth = window.innerWidth;
+  }
+
 }
